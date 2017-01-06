1dd1583d7502fe1544488bd8f4fb6e7a135181ce
Stephanie Zheng <johndoe@example.com>
2017-01-06 15:22:15 -0500

    added to money

--- Money.java
+++ Money.java
@@ -3,8 +3,16 @@ public class Money {
     public static void addProfit(int x){
 	Account.setProfit(Account.getProfit()+x);
     }
+
+    public static void subtractProfit(int x){
+	Account.setProfit(Account.getProfit()-x);
+    }
+
+    public static double cost(int quantity, double price){
+	double cost=quantity*price;
+	return cost;
+    }
+
     
-
-
-
 }
+   
