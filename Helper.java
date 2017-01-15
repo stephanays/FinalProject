697046d509edb99e782d2a40ac560799191578c1
Michela Marchini <mmarchini@stuy.edu>
2017-01-14 23:10:33 -0500

    helper can now read in a file and create an arraylist containing the data of each action (for example a product with all the info needed to construct it is its own arraylist or a restock order is its own arraylist)

--- Helper.java
+++ Helper.java
@@ -2,10 +2,12 @@ import java.io.*;
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.nio.file.Files;
+import java.nio.charset.Charset;
+import java.util.Arrays;
 
 public class Helper{
 
-    public static ArrayList<String> readAccounts(){
+    public static ArrayList<String> readUsers(){
 	ArrayList <String> usernames = new ArrayList <String>();
 	try{
 	    Scanner sc = new Scanner(new File("usernames.txt"));
@@ -21,7 +23,7 @@ public class Helper{
 
     public static boolean userExist(String username){
 	ArrayList <String> users = new ArrayList <String>();
-	users = readAccounts();
+	users = readUsers();
 	for(int index = 0; index < users.size(); index++){
 	    if(users.get(index).equals(username)){
 		return true;
@@ -51,6 +53,37 @@ public class Helper{
 	}
     }
 
+    public static ArrayList<ArrayList<String>> readAccount(String accountName){
+	String filename= accountName+".txt";
+	try{
+	    BufferedReader reader = new BufferedReader
+		(new FileReader(filename));
+	    try{
+	    String accountInfo = reader.readLine();
+	    ArrayList<String> actions = new ArrayList<String>
+		(Arrays.asList(accountInfo.split("/")));
+	    ArrayList<ArrayList<String>> actionsArray = new ArrayList<ArrayList<String>>();
+	    for (int index = 0; index < actions.size(); index++){
+		ArrayList<String> oneAction = new ArrayList<String>
+		    (Arrays.asList(actions.get(index).split(",")));
+		actionsArray.add(oneAction);
+	    }
+	    return actionsArray;
+	    }
+	    catch(IOException e){
+		System.err.println("Input/Output Exception");
+		ArrayList<ArrayList<String>> error = new ArrayList<ArrayList<String>>();
+		return error;
+	    }
+	}
+	catch(FileNotFoundException e){
+	    System.out.println("File not found");
+	    ArrayList<ArrayList<String>> error = new ArrayList<ArrayList<String>>();
+	    return error;
+	}
+    }
+	
+
     public static double cost(int quantity, double price){
 	double cost=quantity*price;
 	return cost;
@@ -58,10 +91,11 @@ public class Helper{
 
     public static void main(String[]args){
 	Helper a = new Helper();
-	System.out.println(a.readAccounts());
+	System.out.println(a.readUsers());
 	System.out.println(a.userExist("pizza"));
 	a.newUser("Mr. K");
-	System.out.println(a.readAccounts());
+	System.out.println(a.readUsers());
+	System.out.println(a.readAccount("tester"));
     }
 	
 
